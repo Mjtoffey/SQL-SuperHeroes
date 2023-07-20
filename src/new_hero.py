a1d8144f3d40186044bbb87e4c54a9e89d37cba8
Mjtoffey <mjtoffey@gmail.com>
2023-07-20 23:57:19 +0000

    updated new_hero.py

--- src/new_hero.py
+++ src/new_hero.py
@@ -2,11 +2,15 @@ from database.connection import execute_query, create_connection
 import psycopg2
 
 def create_new_hero(name, about_me, biography):
+    name = input ("New hero Name:")
+    about_me = input ("About new hero:")
+    biography = input ("New hero Biography:")
     query = """
         INSERT INTO heroes (name, about_me, biography)
         VALUES (%s, %s, %s)
         """
     execute_query(query, (name, about_me, biography))
+    print(f"New Hero successfully created")
 
-create_new_hero("Aang", "Appa, yip yip!", "Just a kid trying to save the world and learn about the elements")
+create_new_hero("","","")
 
